51d559ec8218aca6b7cda413159890960cd65a18
heartyoh <heartyoh@hatiolab.com>
2012-11-06 07:40:53 +0900

    Initial Commit with rails_admin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Termino::Application.config.secret_token = 'fb5c698e286f7ddf96ba99a588d998d5f35deb29c8ea3c7b6a585f1ae64d702089dc3d4661c51dc2c3c35cda79fb943fab1c50f62670adcb11a3093eec808468'
